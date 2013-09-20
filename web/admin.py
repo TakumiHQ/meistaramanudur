fa6b804f084ee7291b57c5acba097cbdb52a744c
Jökull Sólberg Auðunsson <jokull@solberg.is>
2013-09-20 10:02:20 +0000

    Added search to admin

--- web/admin.py
+++ web/admin.py
@@ -3,4 +3,8 @@ from flask.ext.admin.contrib import sqlamodel
 from .models import Applicant
 from .extensions import admin, db
 
-admin.add_view(sqlamodel.ModelView(Applicant, db.session))
+class ApplicantModelView(sqlamodel.ModelView):
+    column_searchable_list = ('name', 'email')
+    column_filters = ('newcomer', )
+
+admin.add_view(ApplicantModelView(Applicant, db.session))
