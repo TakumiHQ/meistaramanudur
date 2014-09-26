05273a985b1f8526c99b97069445dc9701826a3f
Jökull Sólberg Auðunsson <jokull@solberg.is>
2014-09-26 10:47:24 +0000

    Secret key

--- web/app.py
+++ web/app.py
@@ -5,11 +5,13 @@ import datetime
 
 from flask import Flask, current_app, url_for
 
+
 def create_app():
 
     app = Flask(__name__, static_folder="../public")
     app.config.setdefault('STATIC_URL', os.environ.get('STATIC_URL'))
     app.config.setdefault('SQLALCHEMY_DATABASE_URI', os.environ.get('DATABASE_URL'))
+    app.config.setdefault('SECRET_KEY', os.environ.get('SECRET_KEY'))
 
     from web.extensions import configure_extensions
     configure_extensions(app)
@@ -25,6 +27,7 @@ def create_app():
     app.register_blueprint(views)
     return app
 
+
 def static(filename):
     if current_app.debug or current_app.config['STATIC_URL'] is None:
         return url_for('static', filename=filename)
@@ -37,4 +40,3 @@ if __name__ == '__main__':
         from web.extensions import db
         db.create_all()
     app.run(host='0.0.0.0', port=5010, debug=True)
-
